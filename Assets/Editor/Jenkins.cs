09586600274dd22b1b36ec3376f8c5b8f893da86
HyesungKum <rmagptjd77@gmail.com>
2022-12-19 17:37:38 +0900

    initial commit metabox project

---
+++ Assets/Editor/Jenkins.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class Jenkins
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+    static string APP_NAME = "MetaBox";
+
+    [UnityEditor.MenuItem("TestBuild/BuildStart", false, 1)]
+    static void PerformBuild()
+    {
+
+        string target_filename = "Build/" + APP_NAME + ".exe";
+        SCENES = FindEnabledEditorScenes();
+
+        GenericBuild(SCENES, target_filename, BuildTarget.StandaloneWindows64, BuildOptions.None);
+    }
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_filename, BuildTarget build_target, BuildOptions build_options)
+    {
+        BuildPipeline.BuildPlayer(scenes, Directory.GetCurrentDirectory() + "/Build/" + target_filename, build_target, build_options);
+    }
+}
