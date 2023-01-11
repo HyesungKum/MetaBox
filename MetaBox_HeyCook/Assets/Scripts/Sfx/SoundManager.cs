06794b07fc95b25be9f029824c06087117072e01
HyesungKum <rmagptjd77@gmail.com>
2023-01-11 21:38:46 +0900

    add Serialize Dictionary with apply customProperty

--- MetaBox_HeyCook/Assets/Scripts/Sfx/SoundManager.cs
+++ MetaBox_HeyCook/Assets/Scripts/Sfx/SoundManager.cs
@@ -1,54 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
 
-
-[System.Serializable]
-public struct Dic<Tkey,Tvalue>
+public class SoundManager : MonoBehaviour
 {
-    [SerializeField] Tkey key;
-    [SerializeField] Tvalue value;
+    public CustomDictionary<string, string> clips = new();
+
+    private void Update()
+    {
+    }
 }
-
-public class CD<Tkey,Tvalue>
-{
-    public Dictionary<Tkey, Tvalue> dictionary;
-
-    public Dic<Tkey,Tvalue>[] dic;
-}
-
-
-[CreateAssetMenu(menuName = "ScriptableObj/SoundData", fileName = "SoundData")]
-public class SoundData : ScriptableObject
-{
-    public Dic<string, AudioClip>[] dic;
-}
-
-//[CustomEditor(typeof(SoundData))]
-//public class SoundDataEditor : Editor
-//{
-//    private Dictionary<string, AudioClip> clips;
-
-//    private void OnEnable()
-//    {
-//        dictionary = (CustomDictionary)target;
-//    }
-
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
-
-//        if (dictionary.keys.Count > 0)
-//        {
-//            for (int i = 0; i < dictionary.keys.Count; i++)
-//            {
-//                EditorGUILayout.BeginHorizontal();
-
-//                EditorGUILayout.TextField(dictionary.keys[0]);
-//                EditorGUILayout.IntField(dictionary.values[0]);
-
-//                EditorGUILayout.EndHorizontal();
-//            }
-//        }
-//    }
-//}
