c66967f807f275d29f8b606f2997d1237fbaa182
Wally0822 <111326483+Wally171022@users.noreply.github.com>
2023-01-11 11:28:42 +0900

    edited Draw line bug a little more this morning by Wally

--- MetaBox_SketchUP/Assets/Scripts/DrawLine.cs
+++ MetaBox_SketchUP/Assets/Scripts/DrawLine.cs
@@ -24,7 +24,7 @@ public class DrawLine : MonoBehaviour
     GameObject instLine = null;
 
     Stack<GameObject> lineBackStack = new Stack<GameObject>();
-    Stack<GameObject> checkBack = new Stack<GameObject>();
+    Stack<Vertex> checkVertex = new Stack<Vertex>();
 
     int winCount = 0;
 
@@ -37,6 +37,7 @@ public class DrawLine : MonoBehaviour
         vCount = 0;
         verticesCount = FindObjectsOfType<Vertex>().Length;
         clearImg = FindObjectOfType<Clear>();
+
     }
 
     void Update()
@@ -52,40 +53,56 @@ public class DrawLine : MonoBehaviour
 
         if (objCheck)
         {
-            if (objCheck.collider.name.Equals("DrowPoint"))
+            if (objCheck.collider.name.CompareTo("DrowPoint") == 0)
             {
                 if (Input.GetTouch(0).phase == TouchPhase.Began)  // Touch Start
                 {
-                    //checkBack.Clear();
-                    checkBack.Push(objCheck.transform.gameObject);  // drow point Stack
-
                     instLine = ObjectPoolCP.PoolCp.Inst.BringObjectCp(linePrefab); // Object Pool
                     instLine.transform.SetParent(instPos.transform); // Inst Line setParent 
+                    Debug.Log("1 생성됨?? " + instLine);
 
-                    lineBackStack.Push(instLine);  // linStack Stack Add One Object
+                    //lineBackStack.Push(instLine);  // linStack Stack Add One Object
+                    //Debug.Log(" 1) lineBackStack 한개 추가" + lineBackStack.Count);
 
                     startPos = Input.GetTouch(0).position;
                     instLine.transform.position = startPos;
+                    //Debug.Log("## 첫 체크 : " + instLine.transform.localScale);
+
 
                     if (objCheck.transform.TryGetComponent<Vertex>(out collisionVertex) == true)
                     {
+                        //Debug.Log(" 들어감 ?");
                         if (startVertex == null)
                         {
-                            collisionVertex.StartPointColor();
                             tempVertex = collisionVertex; // Start DrowPoint Save
                             startVertex = collisionVertex;
+                            collisionVertex.StartPointColor();
+
+                            checkVertex.Push(collisionVertex);  // drow point Stack
+                            Debug.Log("checkVertex 스택 한개 추가 : " + checkVertex.Count);
 
                             check = startVertex.transform.parent.gameObject; // parent GameObject 
-                            //Debug.Log("parent.name : " + check);
+                                                                             //Debug.Log("parent.name : " + check);
                         }
                         else
                         {
                             if (collisionVertex.GetNodeName().CompareTo(startVertex.GetNodeName()) != 0)
                             {
+                                //Debug.Log("collisionVertex.GetNodeName() :" +collisionVertex.GetNodeName());
+                                //Debug.Log("startVertex.GetNodeName() : " + startVertex.GetNodeName());
+
+                                // === 오브젝트 풀링으로 오브젝트 삭제 변경 하기
                                 Destroy(instLine.gameObject);
+                            }
+                            else if (instLine != null)
+                            {
+                                Debug.Log("## 1 삭제 안됨 ");
+                                lineBackStack.Push(instLine);  // linStack Stack Add One Object
+                                Debug.Log(" 1) lineBackStack 한개 추가" + lineBackStack.Count);
                             }
                         }
                     }
+
                 }
                 if (Input.GetTouch(0).phase == TouchPhase.Moved)
                 {
@@ -99,17 +116,18 @@ public class DrawLine : MonoBehaviour
                             {
                                 if (collisionVertex.GetNodeName().CompareTo(startVertex.GetNextNodeName(i)) == 0)
                                 {
-                                    //Debug.Log("## check name : " + collisionVertex.GetNodeName().CompareTo(startVertex.GetNextNodeName(i)));
-                                    //Debug.Log("collisionVertex :" + collisionVertex);
-
-                                    checkBack.Push(objCheck.transform.gameObject);  // drow point Stack
-
                                     collisionVertex.ColorChange(); // Color Changed 
 
                                     instLine = ObjectPoolCP.PoolCp.Inst.BringObjectCp(linePrefab);
                                     instLine.transform.SetParent(instPos.transform);
+                                    //Debug.Log("2 생성됨?? " + instLine);
 
-                                    lineBackStack.Push(instLine); // linStack Stack Add One Object
+                                    //checkBack.Push(objCheck.transform.gameObject);  // drow point Stack
+                                    //lineBackStack.Push(instLine); // linStack Stack Add One Object
+                                    //Debug.Log(" 2) lineBackStack 한개 추가" + lineBackStack.Count);
+
+                                    checkVertex.Push(collisionVertex);  // drow point Stack
+                                    Debug.Log("checkVertex 스택 한개 추가 : " + checkVertex.Count);
 
                                     startPos = Input.GetTouch(0).position;
                                     instLine.transform.position = startPos;
@@ -117,14 +135,18 @@ public class DrawLine : MonoBehaviour
                                     Vector3 myPos = Input.GetTouch(0).position;
 
                                     instLine.transform.localScale = new Vector2(Vector3.Distance(myPos, startPos), 1);
+                                    //Debug.Log("localScale : " + instLine.transform.localScale);
+
+                                    // localScale 이 0이면 Destroy
                                     instLine.transform.localRotation = Quaternion.Euler(0, 0, AngleInDeg(startPos, myPos));
 
                                     startVertex = collisionVertex;
+
                                     break;
                                 }
                             }
-                            vCount++;
-                            //Debug.Log(vCount + " count!");
+
+                            vCount += 1;
                         }
                     }
                 }
@@ -142,7 +164,15 @@ public class DrawLine : MonoBehaviour
                                 }
                                 else if (i == startVertex.GetNodeLength() - 1)
                                 {
+                                    // === 오브젝트 풀링으로 오브젝트 삭제 변경 하기
                                     Destroy(instLine.gameObject);
+
+                                }
+                                else if (instLine != null)
+                                {
+                                    Debug.Log("## 2 삭제 안됨 ");
+                                    lineBackStack.Push(instLine);  // linStack Stack Add One Object
+                                    Debug.Log(" 2) lineBackStack 한개 추가" + lineBackStack.Count);
                                 }
                             }
                             endVertex = collisionVertex;
@@ -165,44 +195,40 @@ public class DrawLine : MonoBehaviour
         {
             if (instLine != null)
             {
+                // === 오브젝트 풀링으로 오브젝트 삭제 변경 하기
                 Destroy(instLine.gameObject);
 
-                if (endVertex == tempVertex)
-                {
-                    if (clearImg != null)
-                    {
-                        if (check.transform.name == "StarObj1")
-                        {
-                            winCount += 1;
-                            clearImg.ClearIMgObjTwo();
-                        }
-                        else if(check.transform.name == "Obj3")
-                        {
-                            winCount += 1;
-                            clearImg.ClearImgOne();
-                        }
-                        else if (check.transform.name == "Obj2")
-                        {
-                            winCount += 1;
-                            clearImg.ClearIMgObjThree();
-                        }
-                    }
-                }
+                //if (instLine != null)
+                //{
+                //    Debug.Log("## 1 삭제 안됨 ");
+                //    lineBackStack.Push(instLine);  // linStack Stack Add One Object
+                //    Debug.Log(" 3) lineBackStack 한개 추가" + lineBackStack.Count);
+                //}
+                #region
+                //if (endVertex == tempVertex)
+                //{
+                //    if (clearImg != null)
+                //    {
+                //        if (check.transform.name == "StarObj1")
+                //        {
+                //            winCount += 1;
+                //            clearImg.ClearIMgObjTwo();
+                //        }
+                //        else if (check.transform.name == "Obj3")
+                //        {
+                //            winCount += 1;
+                //            clearImg.ClearImgOne();
+                //        }
+                //        else if (check.transform.name == "Obj2")
+                //        {
+                //            winCount += 1;
+                //            clearImg.ClearIMgObjThree();
+                //        }
+                //    }
+                //}
+                #endregion
             }
         }
-    }
-
-    //void CheckFindsh(GameObject obj, string parentName)
-    //{
-    //    if (obj.transform.name == parentName)
-    //    {
-    //        clearImg.ClearIMgObjTwo();
-    //    }
-    //}
-
-    void CheckObjFinsh()
-    {
-
     }
 
 
@@ -210,15 +236,30 @@ public class DrawLine : MonoBehaviour
     {
         if (lineBackStack.Count == 0)
         {
+            lineBackStack.Clear();
+            Debug.Log("lineBackStack 스택 비우기");
             return;
         }
 
         GameObject deleteObj = lineBackStack.Pop();
-        //Debug.Log("## 스택에서 하나 뺴기 : " + deleteObj);
+        Debug.Log("## lineBackStack에서 하나 뺴기 : " + deleteObj);
+        Debug.Log("뺴고 카운트 lineBackStack Count : " + lineBackStack.Count);
+        Destroy(deleteObj.gameObject);
 
-        Destroy(deleteObj);
 
-        GameObject ChangedColor = checkBack.Pop();
+        //GameObject ChangedColor = checkBack.Pop();
+        //Debug.Log("## checkBack 하나 뺴기 : " + ChangedColor);
+        //Debug.Log("빼고난뒤 카운트checkBack Count : " + checkBack.Count);        
+
+        if (checkVertex.Count == 0)
+        {
+            checkVertex.Clear();
+            Debug.Log("checkVertex 스택 비우기");
+            return;
+        }
+        Vertex ChangedColor = checkVertex.Pop();
+        Debug.Log("## checkBack 하나 뺴기 : " + ChangedColor);
+        Debug.Log("빼고난뒤 카운트checkBack Count : " + checkVertex.Count);
         //ChangedColor = checkBack.Pop();
         ChangedColor.GetComponent<Vertex>().BackOriginalColor();
         //Debug.Log("## 컬러 원래대로 돌려줘");
