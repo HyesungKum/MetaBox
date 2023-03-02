82b9bd5c86ac30bae74d45d3f6a1dd4a892e5b8b
Wally0822 <111326483+Wally171022@users.noreply.github.com>
2023-03-02 20:11:59 +0900

    Changed random keWord logic by Wally

--- MetaBox_SketchUP/Assets/Scripts/RandomChoiceKeyWord.cs
+++ MetaBox_SketchUP/Assets/Scripts/RandomChoiceKeyWord.cs
@@ -12,25 +12,18 @@ public class RandomChoiceKeyWord : MonoBehaviour
 
     [Header("[Answer String]")]
     [SerializeField] private string answerKeyWord;
-    [SerializeField] private string koreanAnswerKeyWord;
-    public string AnswerKeyWord { get { return answerKeyWord; } set { answerKeyWord = value; } }
-    public string KoreanAnswerKeyWord { get { return koreanAnswerKeyWord; } set { koreaAnimalOne = value; } }
-
-    [Header("[Other Animal String]")]
-    [SerializeField] private string animalOne = null;
-    [SerializeField] string koreaAnimalOne = null;
-    [SerializeField] private string animalTwo = null;
-    [SerializeField] string koreaAnimalTwo = null;
-    [SerializeField] private string animalThree = null;
-    [SerializeField] string koreaAnimalThree = null;
-
-
-    public string AnimalOne { get { return animalOne; } set { animalOne = value; } }
-    public string AnimalTwo { get { return animalTwo; } set { animalTwo = value; } }
-    public string AnimalThree { get { return animalThree; } set { animalThree = value; } }
 
     public ChoicePanelData AnimalDataContainer = null;
-    public List<string> animalDataList;
+    public List<string> animalNameList = null;
+
+    public string answer { get; set; }
+
+    int answerIndex;
+
+    int answerNum;
+    int butNumTwo;
+    int butNumThree;
+    int butNumFour;
 
     void Awake()
     {
@@ -38,138 +31,114 @@ public class RandomChoiceKeyWord : MonoBehaviour
             AnimalDataContainer = Resources.Load<ChoicePanelData>("Data/AnimalNameData");
 
         #region List Add
-        animalDataList = new List<string>();
-        animalDataList.Add(AnimalDataContainer.Polarbear); // ºÏ±Ø°õ
-        animalDataList.Add(AnimalDataContainer.Reindeer);  // ¼ø·Ï
-        animalDataList.Add(AnimalDataContainer.Penguin);   // Æë±Ï
-
-        animalDataList.Add(AnimalDataContainer.Orca);      // ¹ü°í·¡
-        animalDataList.Add(AnimalDataContainer.Walrus);    // ¹Ù´ÙÄÚ³¢¸®
-        animalDataList.Add(AnimalDataContainer.Dolphin);   // µ¹°í·¡
-
-        animalDataList.Add(AnimalDataContainer.Giraffe);   // ±â¸°
-        animalDataList.Add(AnimalDataContainer.Elephant);  // ÄÚ³¢¸® 
-        animalDataList.Add(AnimalDataContainer.Cheetah);   // Ä¡Å¸
-
-        animalDataList.Add(AnimalDataContainer.Tiger);     // È£¶ûÀÌ
-        animalDataList.Add(AnimalDataContainer.Deer);      // »ç½¿
-        animalDataList.Add(AnimalDataContainer.Rabbit);    // Åä³¢
+        animalNameList = new List<string>();
+        animalNameList.Add(AnimalDataContainer.Polarbear);
+        animalNameList.Add(AnimalDataContainer.Reindeer);
+        animalNameList.Add(AnimalDataContainer.Penguin);
+        animalNameList.Add(AnimalDataContainer.Orca);
+        animalNameList.Add(AnimalDataContainer.Walrus);
+        animalNameList.Add(AnimalDataContainer.Dolphin);
+        animalNameList.Add(AnimalDataContainer.Giraffe);
+        animalNameList.Add(AnimalDataContainer.Elephant);
+        animalNameList.Add(AnimalDataContainer.Cheetah);
+        animalNameList.Add(AnimalDataContainer.Tiger);
+        animalNameList.Add(AnimalDataContainer.Deer);
+        animalNameList.Add(AnimalDataContainer.Rabbit);
         #endregion
 
-        if (animalDataList.Contains(AnswerKeyWord) == true &&
-           animalDataList.Contains(AnimalOne) == true &&
-           animalDataList.Contains(AnimalTwo) == true &&
-           animalDataList.Contains(AnimalThree) == true)
+        int listCount;
+        listCount = animalNameList.Count;
+
+        for (int i = 0; i < listCount; ++i)
         {
-            AnswerKeyWord = koreanAnswerKeyWord;
-            AnimalOne = koreaAnimalOne;
-            AnimalTwo = koreaAnimalTwo;
-            AnimalThree = koreaAnimalThree;
-            StagRandomKeyWord(AnswerKeyWord, AnimalOne, AnimalTwo, AnimalThree);
+            if (animalNameList[i].Contains(answerKeyWord))
+            {
+                answer = animalNameList[i];
+                answerIndex = i;
+            }
         }
 
+        SetButWord();
     }
 
-    void StagRandomKeyWord(string answer, string animalOne, string animalTwo, string animalThree)
+    void SetButWord()
     {
-        #region
-        int random = Random.Range(0, 4);
-        int randomtwo = Random.Range(0, 4);
-        int randomThree = Random.Range(0, 4);
-        int randomFour = Random.Range(0, 4);
+        answerNum = Random.Range(0, 4);
 
-
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 4; ++i)
         {
-            if (random == randomtwo || random == randomThree || random == randomFour)
+            if (answerNum == 0)
             {
-                random -= 1;
-                if (random == -1) random += 4;
+                butNumTwo = 1; butNumThree = 2; butNumFour = 3;
             }
-            else if (randomtwo == random || randomtwo == randomThree || randomtwo == randomFour)
+            if (answerNum == 1)
             {
-                randomtwo -= 1;
-                if (randomtwo == -1) randomtwo += 4;
+                butNumTwo = 0; butNumThree = 2; butNumFour = 3;
             }
-            else if (randomThree == random || randomThree == randomtwo || randomThree == randomFour)
+            if (answerNum == 2)
             {
-                randomThree -= 1;
-                if (randomThree == -1) randomThree += 4;
+                butNumTwo = 0; butNumThree = 1; butNumFour = 3;
             }
-            else if (randomFour == random || randomFour == randomtwo || randomFour == randomThree)
+            if (answerNum == 3)
             {
-                randomFour -= 1;
-                if (randomFour == -1) randomFour += 4;
+                butNumTwo = 0; butNumThree = 1; butNumFour = 2;
             }
         }
 
-        if (random == randomtwo)
+        animalNameList.RemoveAt(answerIndex); // Á¤´äÀ» ¸®½ºÆ®¿¡¼­ »«´Ù
+        int listCount = animalNameList.Count;
+        int randomOne = Random.Range(0, listCount);
+        int randomTwo = Random.Range(0, listCount);
+        int randomThree = Random.Range(0, listCount);
+
+        //Debug.Log("## randomOne : " + randomOne);
+        //Debug.Log("## randomTwo : " + randomTwo);
+        //Debug.Log("## randomThree : " + randomThree);
+
+        if(randomOne == randomTwo || randomOne == randomThree)
         {
-            random -= 2;
-            if (random == -1) random += 3;
-            if (random == -2) random += 4;
+            randomOne += 1;
         }
-        if(random == randomThree)
+        else if(randomTwo == randomOne || randomTwo == randomThree)
         {
-            randomThree -= 2;
-            if (randomThree == -1) randomThree += 3;
-            if (randomThree == -2) randomThree += 4;
+            randomTwo += 1;
         }
-        if(random == randomFour)
+        else if(randomThree == randomOne || randomThree == randomTwo)
         {
-            random -= 2;
-            if(random == -1) random += 3;
-            if(random == -2) random += 4;
-        }
-        if (randomtwo == randomThree)
-        {
-            randomtwo -= 2;
-            if (randomtwo == -1) randomtwo += 3;
-            if(randomtwo == -2) randomtwo += 4;
-        }
-        if(randomThree == randomFour)
-        {
-            randomThree-= 2;
-            if (randomThree == -1) randomFour += 3;
-            if(randomThree== -2) randomFour += 4;
+            randomThree += 1;
         }
 
-        //Debug.Log("$$ random : " + random);
-        //Debug.Log("$$ randomtwo : " + randomtwo);
-        //Debug.Log("$$ randomThree : " + randomThree);
-        //Debug.Log("$$ RandomFour : " + randomFour);
+        //Debug.Log("## randomOne : " + randomOne);
+        //Debug.Log("## randomTwo : " + randomTwo);
+        //Debug.Log("## randomThree : " + randomThree);
 
-        for (int i = 0; i < 4; i++)
+        if (answerNum == 0)
         {
-            if (random == 0) choiceOneButText.text = answer;
-            else if (random == 1) choiceOneButText.text = animalOne;
-            else if (random == 2) choiceOneButText.text = animalTwo;
-            else choiceOneButText.text = animalThree;
+            choiceOneButText.text = answer;
+            choiceTwoButText.text = animalNameList[randomOne];
+            choiceThreeButText.text = animalNameList[randomTwo];
+            choiceFourButText.text = animalNameList[randomThree];
         }
-
-        for (int i = 0; i < 4; i++)
+        else if (answerNum == 1)
         {
-            if (randomtwo == 0) choiceTwoButText.text = answer;
-            else if (randomtwo == 1) choiceTwoButText.text = animalOne;
-            else if (randomtwo == 2) choiceTwoButText.text = animalTwo;
-            else choiceTwoButText.text = animalThree;
+            choiceOneButText.text = animalNameList[randomOne];
+            choiceTwoButText.text = answer;
+            choiceThreeButText.text = animalNameList[randomTwo];
+            choiceFourButText.text = animalNameList[randomThree];
         }
-
-        for (int i = 0; i < 4; i++)
+        else if (answerNum == 2)
         {
-            if (randomThree == 0) choiceThreeButText.text = answer;
-            else if (randomThree == 1) choiceThreeButText.text = animalOne;
-            else if (randomThree == 2) choiceThreeButText.text = animalTwo;
-            else choiceThreeButText.text = animalThree;
+            choiceOneButText.text = animalNameList[randomTwo];
+            choiceTwoButText.text = animalNameList[randomOne];
+            choiceThreeButText.text = answer;
+            choiceFourButText.text = animalNameList[randomThree];
         }
-
-        for (int i = 0; i < 4; i++)
+        else if (answerNum == 3)
         {
-            if (randomFour == 0) choiceFourButText.text = answer;
-            else if (randomFour == 1) choiceFourButText.text = animalOne;
-            else if (randomFour == 2) choiceFourButText.text = animalTwo;
-            else choiceFourButText.text = animalThree;
+            choiceOneButText.text = animalNameList[randomOne];
+            choiceTwoButText.text = animalNameList[randomTwo];
+            choiceThreeButText.text = animalNameList[randomThree];
+            choiceFourButText.text = answer;
         }
-        #endregion
     }
 }
