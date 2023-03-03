b556094af1b6a55553ba6cdd069c4cc09850b74b
HyesungKum <rmagptjd77@gmail.com>
2023-03-03 16:53:34 +0900

    check by Kum

--- Assets/Editor/BeanFontChanger.cs
+++ Assets/Editor/BeanFontChanger.cs
@@ -1,40 +1,40 @@
-using TMPro;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
 
 namespace BeanTool
 {
-    public class BeanFontChanger
+    public class BeanMaterialChanger
     {
-        public const string PATH_FONT_TEXTMESHPRO_M = "Assets/TextMesh Pro/Fonts/Maplestory Bold SDF.Asset";
+        public const string PATH_MATERIAL_URP = "Assets/UrpMaterial.mat";
 
-        [MenuItem("BeanTool/BeanFontChanger")]
-        public static void ChangeFont()
+        [MenuItem("BeanTool/BeanMaterialChanger")]
+        public static void ChangeMaterial()
         {
             GameObject[] rootObj = GetSceneRootObject();
 
-            for(int i = 0; i < rootObj.Length; i++)
+            for (int i = 0; i < rootObj.Length; i++)
             {
                 GameObject go = (GameObject)rootObj[i] as GameObject;
-                Component[] com1 = go.transform.GetComponentsInChildren(typeof(TMP_InputField), true);
-                foreach (TMP_InputField txt in com1)
+                Component[] com1 = go.transform.GetComponentsInChildren(typeof(SpriteRenderer), true);
+                foreach (SpriteRenderer sp in com1)
                 {
-                    txt.fontAsset = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(PATH_FONT_TEXTMESHPRO_M);
+                    sp.material = AssetDatabase.LoadAssetAtPath<Material>(PATH_MATERIAL_URP);
                 }
-                Component[] com2 = go.transform.GetComponentsInChildren(typeof(TextMeshProUGUI), true);
-                foreach(TextMeshProUGUI txt in com2)
+                Component[] com2 = go.transform.GetComponentsInChildren(typeof(Image), true);
+                foreach (Image img in com2)
                 {
-                    txt.font = AssetDatabase.LoadAssetAtPath<TMP_FontAsset>(PATH_FONT_TEXTMESHPRO_M);
+                    img.material = AssetDatabase.LoadAssetAtPath<Material>(PATH_MATERIAL_URP);
                 }
             }
-
         }
+
         static GameObject[] GetSceneRootObject()
         {
-            Scene curscene = SceneManager.GetActiveScene();
+            Scene curscene = EditorSceneManager.GetActiveScene();
             return curscene.GetRootGameObjects();
         }
-
     }
 }
